6f5c99cf23fdd5ca465098d2ddc519d3387ac020
Miriã <71527774+miri12345@users.noreply.github.com>
2024-09-25 20:36:16 -0300

    Add files via upload

---
+++ Projeto Hospital Exemplar/CREATE/quartos.js
@@ -0,0 +1,12 @@
+db.quartos.insertMany([
+    { numero: "101", tipo: "Apartamento", valor_diario: 300
+    },
+    { numero: "102", tipo: "Quarto Duplo", valor_diario: 200
+    },
+    { numero: "103", tipo: "Enfermaria", valor_diario: 150
+    },
+    { numero: "104", tipo: "Enfermaria", valor_diario: 150
+    },
+    { numero: "105", tipo: "Apartamento", valor_diario: 300
+    }
+]);
